91e3774a92844b014da13ab4eda08ef09838f056
Migarca <migarciacampos@gmail.com>
2024-10-27 18:21:52 +0100

    add configmodule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const logger = new Logger('Bootstrap');
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
+  logger.verbose(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
